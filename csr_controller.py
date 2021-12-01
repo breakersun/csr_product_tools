467c797052a4be03d0001e4a8fc4349ddd8821a0
Leo Sun <leosunsl@outlook.com>
2021-12-01 15:55:40 +0800

    add CsconfigController

--- csr_controller.py
+++ csr_controller.py
@@ -102,11 +102,46 @@ class E2cmdController(CSRControllerBase):
         return ' '.join(words)
 
 
+class CsconfigController(CSRControllerBase):
+    def __init__(self):
+        super().__init__('csconfigcmd.exe')
+        self.check_path()
+
+    def dump(self, file_name, offset=0x4200):
+        if offset:
+            cmd = shlex.split(f'dump {file_name}.keyr -otauappoffset 0x{offset:x}', posix=False)
+        else:
+            cmd = shlex.split(f'dump {file_name}.keyr', posix=False)
+        return self.executor(cmd)
+
+    def get_bdaddr(self, keyr_file):
+        bdaddr_regx = re.compile(r'&BDADDR\s=\s([0-9a-f]{4})\s([0-9a-f]{4})\s([0-9a-f]{4})')
+        with open(keyr_file, 'r') as f:
+            keyr = f.read()
+            m = bdaddr_regx.search(keyr)
+            if m:
+                return ''.join(m.group(3, 2, 1))
+
+    def get_ctune(self, keyr_file):
+        ctune_regx = re.compile(r'&CRYSTAL_FTRIM\s=\s([0-9a-f]{4})')
+        with open(keyr_file, 'r') as f:
+            keyr = f.read()
+            m = ctune_regx.search(keyr)
+            if m:
+                return m.group(1)
+
+
 if __name__ == '__main__':
     FORMAT = "%(levelname)7s %(asctime)s [%(filename)13s:%(lineno)4d] %(message)s"
     DATEFMT = "%H:%M:%S"
     logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt=DATEFMT)
     logging.getLogger(__name__).setLevel(logging.DEBUG)
 
-    tester = E2cmdController()
-    print(tester.read_words(0x414a, 20))
+    # e2cmd = E2cmdController()
+    # print(e2cmd.read_words(0x414a, 20))
+    csconfig = CsconfigController()
+    csconfig.dump('key', offset=0)
+    print(csconfig.get_bdaddr('key.keyr'))
+    print(csconfig.get_ctune('key.keyr'))
+    # remove key.keyr with pathlib
+    # pathlib.Path('key.keyr').unlink()
